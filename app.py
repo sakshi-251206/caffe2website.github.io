437fe5cfe104d7dc5f8bdc06eee7deb55d667b63
sakshi2512 <parandesakshi2005@gmail.com>
2026-01-28 17:12:22 +0530

    Change default port from 5000 to 8080

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def logout():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
